cae7921c4d83299b504b0640264a8ecaff57e959
GuLin <2079720947@qq.com>
2021-05-17 23:51:15 +0800

    Eureka分布式集群配置

--- microservicecloud-eureka-7003/src/main/java/com/example/springcloud/EurekaServer7003App.java
+++ microservicecloud-eureka-7003/src/main/java/com/example/springcloud/EurekaServer7003App.java
@@ -5,16 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * eurekaServer 7001 服务
+ * eurekaServer 7003 服务
  * 1.@EnableEurekaServer 服务启动类，接受其他服务注册进来
  * @author GuLin
- * @create 2021-05-16 21:22
+ * @create 2021-05-17 22:55
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServer7001App {
+public class EurekaServer7003App {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer7001App.class,args);
+        SpringApplication.run(EurekaServer7003App.class,args);
     }
 }
--- microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.example.springcloud.controller;
 import com.example.springcloud.entities.Dept;
 import com.example.springcloud.service.IDeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +20,11 @@ public class DeptController {
 
     @Autowired
     private IDeptService deptService;
+    /**
+     * 服务发现
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -32,6 +39,19 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list() {
         return deptService.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
